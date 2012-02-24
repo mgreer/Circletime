34bde8486f98127fe621495f2d4ac1be11105a9b
Michael Greer <michael.greer@gmail.com>
2012-02-23 18:22:03 -0800

    invite user to circle prior in invite process

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -2,8 +2,14 @@ class Users::InvitationsController < Devise::InvitationsController
 
   # POST /resource/invitation
   def create
-    self.resource = resource_class.invite!(params[resource_name], current_inviter)
-    self.resource.memberships(params[:circle])
+    begin
+      #create the user, with invite info
+      self.resource = resource_class.invite!(params[resource_name], current_inviter)
+      #add to the inviter's circle
+      self.resource.memberships.create(:circle => current_inviter.circle)
+    rescue
+      logger.error { "throw an error about a duplicate membership invite" }
+    end
 
     if resource.errors.empty?
       set_flash_message :notice, :send_instructions, :email => self.resource.email
