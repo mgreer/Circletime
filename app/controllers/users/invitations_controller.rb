7ba775e5e68821eea3a13c36872207930ddca46d
Michael Greer <michael.greer@gmail.com>
2012-02-24 13:24:09 -0800

    invitation updates

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -2,20 +2,29 @@ class Users::InvitationsController < Devise::InvitationsController
 
   # POST /resource/invitation
   def create
-    begin
-      #create the user, with invite info
-      self.resource = resource_class.invite!(params[resource_name], current_inviter)
-      #add to the inviter's circle
-      self.resource.memberships.create(:circle => current_inviter.circle)
-    rescue
-      logger.error { "throw an error about a duplicate membership invite" }
-    end
+    #create the user, with invite info
+    self.resource = resource_class.invite!(params[resource_name], current_inviter)
+    #add to the inviter's circle
+    @membership = self.resource.memberships.create(:circle => current_inviter.circle)
 
-    if resource.errors.empty?
+    if resource.errors.empty? and @membership.errors.empty?
       set_flash_message :notice, :send_instructions, :email => self.resource.email
       respond_with resource, :location => after_invite_path_for(resource)
     else
       respond_with_navigational(resource) { render :new }
+    end
+  end
+
+  # PUT /resource/invitation
+  def update
+    @user = :user.accept_invitation!(params[resource_name])
+
+    if resource.errors.empty?
+      set_flash_message :notice, :updated
+      sign_in(resource_name, resource)
+      respond_with resource, :location => after_accept_path_for(resource)
+    else
+      respond_with_navigational(resource){ render :edit }
     end
   end
   
