611240eac8c705c45e42679655008ec23b0de62f
Michael Greer <michael.greer@gmail.com>
2012-05-02 23:05:03 -0700

    identify local user objects from Facebook kid!

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def find_user_from_facebook( uid )
+    uid = "#{uid}"
+    uid.slice!("uid")
+    Rails.logger.info("-------uid #{uid}")
+    authentication = Authentication.find :all, :conditions => "provider = 'facebook' AND uid = '#{uid}' "
+    Rails.logger.info("-------Authentication #{authentication}")
+    if authentication.first
+      authentication.first.user
+    else
+      return false
+    end
+  end
+
 end
