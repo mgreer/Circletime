0f7e04e2569d4c3c9d90e5bb1d140b36c675bfcc
Michael Greer <michael.greer@gmail.com>
2012-06-20 13:49:14 -0700

    re-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 WorkUnit.create([{ :name => 'day', :hours => 24 }, { :name => 'hour', :hours => 1 }])
-JobType.create( :name => 'babysitter', :work_unit => WorkUnit.find_by_name( 'hour' ), :stars => 1, :is_misc => false )
-JobType.create( :name => 'petsitter', :work_unit => WorkUnit.find_by_name( 'day' ), :stars => 1, :is_misc => false )
-JobType.create( :name => 'favor', :work_unit => WorkUnit.find_by_name( 'day' ), :stars => 1, :is_misc => true )
+JobType.create( :name => 'babysitter', :past => "babysat", :future => "will babysit", :work_unit => WorkUnit.find_by_name( 'hour' ), :stars => 1, :is_misc => false )
+JobType.create( :name => 'petsitter', :past => "petsat", :future => "will petsit", :work_unit => WorkUnit.find_by_name( 'day' ), :stars => 1, :is_misc => false )
+JobType.create( :name => 'favor', :past => "did a favor", :future => "will do a favor", :work_unit => WorkUnit.find_by_name( 'day' ), :stars => 1, :is_misc => true )
